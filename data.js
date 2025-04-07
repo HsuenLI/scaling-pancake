3d712843b71984f47c512c4f10d906ba41b35bb5
HsuenLI <40624553+HsuenLI@users.noreply.github.com>
2025-04-08 00:16:53 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,501 @@
+export const data = [
+  {
+    "name": "解脂酶(Lipase)",
+    "price": 400,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "鐵蛋白(Ferritin)",
+    "price": 400,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "膽素脂酶(Cholinesterase)",
+    "price": 400,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "小紅管 ＋1",
+    "tubeColorClass": "tube-red"
+  },
+  {
+    "name": "B型肝炎表面抗原(HBsAg)、B型肝炎表面抗體 (Anti-HBs)",
+    "price": 900,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "癌胚胎抗原 CEA",
+    "price": 600,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "胰臟癌 CA-199",
+    "price": 800,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "睪丸癌 β-HCG",
+    "price": 800,
+    "specimen": "血液",
+    "gender": "男性",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "絨毛膜癌 β-HCG",
+    "price": 600,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "肺腺癌篩檢 Cyfra21-1",
+    "price": 1000,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "攝護腺超音波",
+    "price": 900,
+    "specimen": "影像",
+    "gender": "男性",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "男性荷爾蒙檢測",
+    "price": 1000,
+    "specimen": "血液",
+    "gender": "男性",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "高敏感度C反應蛋白 Hs-CRP",
+    "price": 800,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "心臟功能檢查 CK-MB",
+    "price": 800,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "同半胱胺酸 Homocysteine",
+    "price": 1000,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "紫黃管 ＋1",
+    "tubeColorClass": "tube-purpleyellow"
+  },
+  {
+    "name": "超壞膽固醇 small-dense LDL",
+    "price": 1000,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "甲狀腺功能 T3、T4、TSH",
+    "price": 1000,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "甲狀腺球蛋白 Thyroglobulin",
+    "price": 400,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "類風濕因子 RA",
+    "price": 400,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "電解質 Na、K、Cl、Ca、P",
+    "price": 300,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "過敏體質檢查 (IgE)",
+    "price": 700,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "酒精代謝基因檢測",
+    "price": 2500,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "大紫管 ＋1",
+    "tubeColorClass": "tube-purple"
+  },
+  {
+    "name": "遺傳性聽損基因檢測",
+    "price": 4500,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "ApoE阿茲海默症風險基因檢測",
+    "price": 3000,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "大紫管 ＋1",
+    "tubeColorClass": "tube-purple"
+  },
+  {
+    "name": "藥害基因檢測 (HLA-B1502)",
+    "price": 3500,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "婚前健檢項目(男性)-地中海型貧血(電泳)、蠶豆症G6PD、男性荷爾蒙Testosterone",
+    "price": 2200,
+    "specimen": "血液",
+    "locked": true,
+    "gender": "不限",
+    "tube": " 小紫管 ＋2",
+    "tubeColorClass": "tube-purple"
+  },
+  {
+    "name": "婚前健檢項目(女性)-地中海型貧血(電泳)、蠶豆症G6PD、德國麻疹Rubella IgG、水痘抗體",
+    "price": 2200,
+    "specimen": "血液",
+    "locked": true,
+    "gender": "不限",
+    "tube": " 小紫管 ＋2",
+    "tubeColorClass": "tube-purple"
+  },
+  {
+    "name": "地中海型貧血(電泳)",
+    "price": 600,
+    "specimen": "血液",
+    "locked": true,
+    "gender": "不限",
+    "tube": " 小紫管 ＋1",
+    "tubeColorClass": "tube-purple"
+  },
+  {
+    "name": "蠶豆症G6PD",
+    "price": 600,
+    "specimen": "血液",
+    "locked": true,
+    "gender": "不限",
+    "tube": " 小紫管 ＋1",
+    "tubeColorClass": "tube-purple"
+  },
+  {
+    "name": "德國麻疹Rubella IgG",
+    "price": 600,
+    "specimen": "血液",
+    "locked": true,
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "水痘抗體",
+    "price": 600,
+    "specimen": "血液",
+    "locked": true,
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "骨盆腔超音波",
+    "price": 900,
+    "specimen": "影像",
+    "gender": "女性",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "甲狀腺超音波",
+    "price": 900,
+    "specimen": "影像",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "乳房超音波",
+    "price": 900,
+    "specimen": "影像",
+    "gender": "女性",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "頸動脈超音波",
+    "price": 900,
+    "specimen": "影像",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "Anti-ENA抗體(CTD screen)",
+    "price": 600,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "大紅管 ＋1",
+    "tubeColorClass": "tube-red"
+  },
+  {
+    "name": "A型肝炎抗體(HAV-IgG)",
+    "price": 500,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "B型肝炎e抗原(HBeAg)",
+    "price": 500,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "C型肝炎抗體(Anti-HCV)",
+    "price": 500,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "醣化血色素 (HbA1C)",
+    "price": 700,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "小紫管 ＋1",
+    "tubeColorClass": "tube-purple"
+  },
+  {
+    "name": "肺癌(NSE)",
+    "price": 800,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "游離四碘甲狀腺素(Free T4)",
+    "price": 800,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "鱗狀上皮細胞癌(SCC)",
+    "price": 1000,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "鼻咽癌(EBV-IgA)",
+    "price": 800,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "卵巢癌(CA-125)",
+    "price": 800,
+    "specimen": "血液",
+    "gender": "女性",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "胃癌(CA72-4)",
+    "price": 800,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "過敏原檢測-36項",
+    "price": 3000,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "大紅管 ＋1",
+    "tubeColorClass": "tube-red"
+  },
+  {
+    "name": "過敏原檢測-50項",
+    "price": 3000,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "大紅管 ＋2",
+    "tubeColorClass": "tube-red"
+  },
+  {
+    "name": "過敏原檢測-100項",
+    "price": 3000,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "大紅管 ＋3",
+    "tubeColorClass": "tube-red"
+  },
+  {
+    "name": "維生素D3/25 OH Vitamin D3",
+    "price": 1000,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "泌乳激素(Prolactin)",
+    "price": 400,
+    "specimen": "血液",
+    "gender": "女性",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "黃體素(Progesterone)(女性)",
+    "price": 400,
+    "specimen": "血液",
+    "gender": "女性",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "女性荷爾蒙 (E2.LH.FSH)",
+    "price": 1000,
+    "specimen": "血液",
+    "gender": "女性",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "缺鐵性貧血(TIBC.Iron.Ferritin)",
+    "price": 1200,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "後天免疫缺乏症候群(HIV)",
+    "price": 600,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  },
+  {
+    "name": "梅毒反應(VDRL)",
+    "price": 300,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "大紅管 ＋1",
+    "tubeColorClass": "tube-red"
+  },
+  {
+    "name": "C13尿素呼氣檢查幽門桿菌",
+    "price": 1500,
+    "specimen": "氣體",
+    "gender": "不限",
+    "tube": "吹氣試驗 ＋1",
+    "tubeColorClass": "tube-lightblue"
+  },
+  {
+    "name": "胰島素(Insulin)、胰島素阻抗",
+    "price": 800,
+    "specimen": "血液",
+    "gender": "不限",
+    "tube": "none",
+    "tubeColorClass": "none"
+  }
+]
+
+export const groupData = {
+  A: {
+    items: ["解脂酶(Lipase)", "鐵蛋白(Ferritin)", "膽素脂酶(Cholinesterase)", "B型肝炎表面抗原(HBsAg)、B型肝炎表面抗體 (Anti-HBs)"],
+    price: 1800
+  },
+  B1: {
+    items: ["癌胚胎抗原 CEA", "胰臟癌 CA-199", "睪丸癌 β-HCG", "肺腺癌篩檢 Cyfra21-1"],
+    price: 2000
+  },
+  B2: {
+    items: ["癌胚胎抗原 CEA", "胰臟癌 CA-199", "絨毛膜癌 β-HCG", "肺腺癌篩檢 Cyfra21-1"],
+    price: 2000
+  },
+  C: {
+    items: ["高敏感度C反應蛋白 Hs-CRP", "心臟功能檢查 CK-MB", "同半胱胺酸 Homocysteine", "超壞膽固醇 small-dense LDL"],
+    price: 3000
+  },
+  D: {
+    items: [
+      "甲狀腺功能 T3、T4、TSH",
+      "甲狀腺球蛋白 Thyroglobulin",
+      "類風濕因子 RA",
+      "電解質 Na、K、Cl、Ca、P"
+    ],
+    price: 1800
+  }
+};
+
+export const groupTitleMap = {
+  A: 'A 式',
+  B1: 'B1 式',
+  B2: 'B2 式',
+  C: 'C 式',
+  'marriage': '婚前健檢項目',
+  'others': '其他項目'
+};
